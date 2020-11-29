7c05c8cd98b6acf5019cb176a639d9b8ba155dd9
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-11-29 11:04:19 +0700

    fixing main.go (setup db and run server)

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"go-mpnj/api"
+	"net/http"
+
+	"github.com/go-rel/rel"
+	"github.com/go-rel/rel/adapter/sqlite3"
+	_ "github.com/mattn/go-sqlite3"
+	"go.uber.org/zap"
+)
+
+var (
+	logger, _ = zap.NewProduction(zap.Fields(zap.String("type", "main")))
+)
+
 func main() {
-	return
+	var (
+		// ctx        = context.Background()
+		port       = 3000
+		repository = initRepository()
+		mux        = api.NewMux(repository)
+		server     = http.Server{
+			Addr:    ":3000",
+			Handler: mux,
+		}
+	)
+
+	fmt.Println(port)
+
+	logger.Info("server starting: http://localhost" + server.Addr)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		logger.Fatal("server error", zap.Error(err))
+	}
+}
+
+func initRepository() rel.Repository {
+	var (
+		logger, _ = zap.NewProduction(zap.Fields(zap.String("type", "repository")))
+		dsn       = "mpnj.db?_foreign_keys=1&_loc=Local"
+	)
+
+	adapter, err := sqlite3.Open(dsn)
+	if err != nil {
+		logger.Fatal(err.Error(), zap.Error(err))
+	}
+
+	repository := rel.New(adapter)
+
+	return repository
 }
