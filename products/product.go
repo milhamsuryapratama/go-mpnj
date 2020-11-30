55e881bc60dfcfbd7682161b795d164314be9d38
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-11-30 14:23:51 +0700

    create domain product

---
+++ products/product.go
@@ -0,0 +1,21 @@
+package products
+
+import "time"
+
+// Product ...
+type Product struct {
+	ID           int       `json:"id"`
+	ProductName  string    `json:"product_name"`
+	Slug         string    `json:"slug"`
+	Weight       int       `json:"weight"`
+	CapitalPrice int       `json:"capital_price"`
+	SellingPrice int       `json:"selling_price"`
+	Discount     int       `json:"discount"`
+	Stock        int       `json:"stock"`
+	Notes        string    `json:"notes"`
+	Wishlist     int       `json:"wishlist"`
+	Sold         int       `json:"sold"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	CategoryID   int       `json:"category_id"`
+}
